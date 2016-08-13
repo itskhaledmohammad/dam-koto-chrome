31f5a78455ad9b02a9b0b70ef658db9b22f64c8e
Khaled Mohammad <itskhaledmohammad@gmail.com>
2016-08-14 01:06:47 +0600

    Initial Commit

---
+++ background.js
@@ -0,0 +1,17 @@
+/* This functions converts the given 
+ * currency to required currency. 
+ */
+function getRate() {
+    return 80.5;
+}
+$(document).ready(function() {
+    $(".a-color-price").each(function() {
+        $(this).html($(this).html().replace(/&nbsp;/gi,'')); // Replacing the current trailing spaces.
+        var current =  $(this).html(); // Saving the current text
+        var currency = current;
+        var convertedRate = Number(currency.replace(/[^0-9\.]+/g,"")).toFixed(1) * getRate();// Calculating the coversion.
+        $(this).text(current + "(TK. " + convertedRate + ")");
+    });
+});
+
+
